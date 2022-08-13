6729564b8196859e1cacc24018cf3a3c5e48d4a5
clownyCluster <pokharelrupace@gmail.com>
2022-08-13 19:17:51 +0545

    Assets added

--- lib/screens/splash/splashState.dart
+++ lib/screens/splash/splashState.dart
@@ -3,48 +3,55 @@ import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
+import '../../constants/constant.dart';
+import '../../globalTokens.dart';
+import '../../main.dart';
+import '../addAndManage/addAndManage.dart';
+import '../home/home.dart';
+import '../register/register.dart';
+
 
 class SplashState extends ChangeNotifier {
   SplashState(context) {
-    // Timer(Duration(seconds: 1),
-    //     () => Navigator.pushNamed(context, AddAndManage.id));
+    Timer(Duration(seconds: 1),
+        () => Navigator.pushNamed(context, AddAndManage.id));
     // checkAccessToken();
   }
   Dio dio = Dio();
-  // checkAccessToken() {
-  //   Future.delayed(Duration(seconds: 0), () {
-  //     // var checkToken = savedAccessToken;
-  //     final savedAccessToken = token.read("accessToken");
+  checkAccessToken() {
+    Future.delayed(Duration(seconds: 0), () {
+      // var checkToken = savedAccessToken;
+      final savedAccessToken = token.read("accessToken");
 
-  //     if (savedAccessToken == null) {
-  //       navigatorKey.currentState!
-  //           .pushNamedAndRemoveUntil(RegisterScreen.id, (route) => false);
-  //     } else {
-  //       validAccessToken();
-  //     }
-  //   });
-  //   notifyListeners();
-  // }
+      if (savedAccessToken == null) {
+        navigatorKey.currentState!
+            .pushNamedAndRemoveUntil(RegisterScreen.id, (route) => false);
+      } else {
+        validAccessToken();
+      }
+    });
+    notifyListeners();
+  }
 
-  // bool isError = false;
+  bool isError = false;
 
-  // validAccessToken() async {
-  //   // print('object');
-  //   try {
-  //     final savedAccessToken = token.read("accessToken");
-  //     print(savedAccessToken);
+  validAccessToken() async {
+    // print('object');
+    try {
+      final savedAccessToken = token.read("accessToken");
+      print(savedAccessToken);
 
-  //     await dio.get('$url/api/v1/auth/profile',
-  //         options:
-  //             Options(headers: {'Authorization': 'Bearer $savedAccessToken'}));
-  //     print(savedAccessToken);
+      await dio.get('$url/api/v1/auth/profile',
+          options:
+              Options(headers: {'Authorization': 'Bearer $savedAccessToken'}));
+      print(savedAccessToken);
 
-  //     navigatorKey.currentState!
-  //         .pushNamedAndRemoveUntil(Home.id, (route) => false);
-  //   } on DioError catch (e) {
-  //     print(e.response);
-  //     isError = true;
-  //     notifyListeners();
-  //   }
-  // }
+      navigatorKey.currentState!
+          .pushNamedAndRemoveUntil(Home.id, (route) => false);
+    } on DioError catch (e) {
+      print(e.response);
+      isError = true;
+      notifyListeners();
+    }
+  }
 }
